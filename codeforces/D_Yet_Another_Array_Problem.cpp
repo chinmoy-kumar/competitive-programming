16b8f33a761491589dd95148f12598d3e41f771d
chinmoy-kumar <chinmoy.coder@gmail.com>
2025-11-07 00:35:06 +0600

    added codeforces problem

---
+++ codeforces/D_Yet_Another_Array_Problem.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<ll> v(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+
+        bool flag = 0;
+        for (ll i = 2; i <= 100; i++)
+        {
+            for (ll j = 0; j < n; j++)
+            {
+                if(__gcd(i, v[j]) == 1)
+                {
+                    cout << i << nl;
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag) break;
+        }
+        
+    }
+
+    return 0;
+}
