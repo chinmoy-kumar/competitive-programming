2e51a7a7554902fc8643100c026479a3c3b91f54
chinmoy-kumar <chinmoy.coder@gmail.com>
2026-01-27 00:01:18 +0600

    modify codeforces problem

--- codeforces/A_Line_Trip.cpp
+++ codeforces/A_Line_Trip.cpp
@@ -1,3 +1,9 @@
+/*=====================================================
+Author: Chinmoy Kumar Tirtho
+Platform: Codeforces
+Problem: 1901A - Line Trip
+URL: https://codeforces.com/problemset/problem/1901/A
+=====================================================*/
 #include <bits/stdc++.h>
 #define ll long long int
 #define nl '\n'
@@ -10,7 +16,25 @@ int main(){
     cin >> t;
     while(t--)
     {
-        
+        int n, x;
+        cin >> n >> x;
+        vector<int>v;
+        v.push_back(0);
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            v.push_back(a);
+        }
+        int mx_fuel = (x - v[n])*2;
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = v[i+1] - v[i];
+            if(mx_fuel < temp) mx_fuel = temp;
+        }
+
+        cout << mx_fuel << nl;
     }
 
     return 0;
