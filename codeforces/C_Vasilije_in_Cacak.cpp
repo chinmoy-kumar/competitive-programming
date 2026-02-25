edcfb1aee605689a08c67fc4590615451a0a4f08
chinmoy-kumar <chinmoy.coder@gmail.com>
2026-02-25 23:36:49 +0600

    modified a codeforces problem

--- codeforces/C_Vasilije_in_Cacak.cpp
+++ codeforces/C_Vasilije_in_Cacak.cpp
@@ -1,3 +1,9 @@
+/*=====================================================
+Author: Chinmoy Kumar Tirtho
+Platform: Codeforces
+Problem: C. Vasilije in Cacak
+URL: https://codeforces.com/contest/1878/problem/C
+=====================================================*/
 #include <bits/stdc++.h>
 #define ll long long int
 #define nl '\n'
@@ -6,7 +12,22 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        ll n, k;
+        ll x;
+        cin >> n >> k >> x;
+        
+        ll smSum = 0, lgSum = 0;
+        smSum = k*(k+1)/2;
+        lgSum = k*(2*n-k+1)/2;
+        
+        if(smSum <= x && lgSum >= x) cout << "YES" << nl;
+        else cout << "NO" << nl;
+
+    }
 
     return 0;
 }
